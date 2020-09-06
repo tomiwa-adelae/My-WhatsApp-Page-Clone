f24c6f573975e4bb52018a0f6cc8f13c269a75c9
Tomiwa Adelae <tommedia6001@gmail.com>
2020-09-06 17:48:06 -0200

    Added the contact section

--- dist/js/app.js
+++ dist/js/app.js
@@ -4,18 +4,23 @@ const userTwoForm = document.querySelector('.user-2-form');
 const messages = document.querySelector('.messages');
 const userOneInput = document.querySelector('.user-1');
 const userTwoInput = document.querySelector('.user-2');
-const sendBtn = document.querySelector('.user-1-btn');
+const sendOneBtn = document.querySelector('.user-1-btn');
+const sendTwoBtn = document.querySelector('.user-2-btn');
+const headerName = document.querySelector('.header-name');
+const contactSection = document.querySelector('.contact-section');
+const closeContactBtn = document.querySelector('.info-burger');
 
 // EVENTS
 userOneForm.addEventListener('submit', userOneFunction);
 userTwoForm.addEventListener('submit', userTwoFunction);
+headerName.addEventListener('click', showContactFunction);
+closeContactBtn.addEventListener('click', showContactFunctionBtn);
 
 // FUNCTIONS
 function userOneFunction(e) {
    e.preventDefault();
 
    if (userOneInput.value === '') {
-      console.log('Empty Message');
    } else {
       // Grab the text in the input
       const userOneText = userOneInput.value;
@@ -36,7 +41,6 @@ function userTwoFunction(e) {
    e.preventDefault();
 
    if (userTwoInput.value === '') {
-      console.log('Empty Message');
    } else {
       // Grab the text in the input
       const userTwoText = userTwoInput.value;
@@ -51,4 +55,13 @@ function userTwoFunction(e) {
       // Clear the field
       userTwoInput.value = '';
    }
+}
+
+function showContactFunction(e) {
+   // Add the class of show contact section
+   contactSection.classList.toggle('show-contact-section');
+}
+
+function showContactFunctionBtn(e) {
+   contactSection.classList.toggle('show-contact-section');
 }
